e2740e15e5daeffd4a83b2ac1a6789ca0cf802b7
Jack Haek <jackjhaek@gmail.com>
2024-09-03 14:49:23 -0700

    get navigation working

---
+++ frontend/lib/routes/navigation.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+class Destination {
+  const Destination(this.label, this.icon, this.selectedIcon);
+
+  final String label;
+  final Widget icon;
+  final Widget selectedIcon;
+}
+
+const List<Destination> destinations = <Destination>[
+  Destination(
+      'Dashboard', Icon(Icons.dashboard_outlined), Icon(Icons.dashboard)),
+  Destination(
+      'FactoryPlanner', Icon(Icons.factory_outlined), Icon(Icons.factory)),
+  Destination(
+      'PowerPlanner', Icon(Icons.bolt_outlined), Icon(Icons.bolt)),
+  Destination(
+      "ResourceBalance", Icon(Icons.balance_outlined), Icon(Icons.balance)),
+  Destination(
+      "Statistics", Icon(Icons.trending_up_outlined), Icon(Icons.trending_up)),
+  Destination(
+      "Settings", Icon(Icons.settings_outlined), Icon(Icons.settings))
+];
+
+class NavigationDrawerComponent extends StatelessWidget {
+  const NavigationDrawerComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const NavigationDrawerWidget(),
+    );
+  }
+}
+
+class NavigationDrawerWidget extends StatefulWidget {
+  const NavigationDrawerWidget({super.key});
+
+  @override
+  State<NavigationDrawerWidget> createState() =>
+      _NavigationDrawerWidgetState();
+}
+
+class _NavigationDrawerWidgetState extends State<NavigationDrawerWidget> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int screenIndex = 0;
+  late bool showNavigationDrawer;
+
+  void handleScreenChanged(int selectedScreen) {
+    setState(() {
+      screenIndex = selectedScreen;
+    });
+  }
+
+  void openDrawer() {
+    scaffoldKey.currentState!.openEndDrawer();
+  }
+
+  Widget buildBottomBarScaffold() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Page Index = $screenIndex'),
+          ],
+        ),
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: screenIndex,
+        onDestinationSelected: (int index) {
+          setState(() {
+            screenIndex = index;
+          });
+        },
+        destinations: destinations.map(
+              (Destination destination) {
+            return NavigationDestination(
+              label: destination.label,
+              icon: destination.icon,
+              selectedIcon: destination.selectedIcon,
+              tooltip: destination.label,
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
+
+  Widget buildDrawerScaffold(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      body: SafeArea(
+        bottom: false,
+        top: false,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: NavigationRail(
+                minWidth: 50,
+                destinations: destinations.map(
+                      (Destination destination) {
+                    return NavigationRailDestination(
+                      label: Text(destination.label),
+                      icon: destination.icon,
+                      selectedIcon: destination.selectedIcon,
+                    );
+                  },
+                ).toList(),
+                selectedIndex: screenIndex,
+                useIndicator: true,
+                onDestinationSelected: (int index) {
+                  setState(() {
+                    screenIndex = index;
+                  });
+                },
+              ),
+            ),
+            const VerticalDivider(thickness: 1, width: 1),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text('Page Index = $screenIndex'),
+                  ElevatedButton(
+                    onPressed: openDrawer,
+                    child: const Text('Open Drawer'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      endDrawer: NavigationDrawer(
+        onDestinationSelected: handleScreenChanged,
+        selectedIndex: screenIndex,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(28, 16, 16, 10),
+            child: Text(
+              'Header',
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+          ),
+          ...destinations.map(
+                (Destination destination) {
+              return NavigationDrawerDestination(
+                label: Text(destination.label),
+                icon: destination.icon,
+                selectedIcon: destination.selectedIcon,
+              );
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+            child: Divider(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    showNavigationDrawer = MediaQuery.of(context).size.width >= 450;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return showNavigationDrawer
+        ? buildDrawerScaffold(context)
+        : buildBottomBarScaffold();
+  }
+}
